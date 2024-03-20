b5cacb683f256dc65f693e1f22f36d438ee7d35f
cheduvenkatesh <cheduvenkatesh@gmail.com>
2024-03-20 19:54:09 +0530

    to upload upper

--- demo.java
+++ demo.java
@@ -2,7 +2,7 @@ public class demo {
     
     public static void main(String[] args) {
         
-        System.out.println("this is a java code i want to push it into UpperCaseProgram Repository");
+        System.out.println("this is a java code i want to push it into UpperCaseProgram Repository l");
 
     }
 }
