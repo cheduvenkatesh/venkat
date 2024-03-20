cb5a71666a391ffdb7d0c68eb4915313d1325f38
cheduvenkatesh <cheduvenkatesh@gmail.com>
2024-03-20 19:31:13 +0530

    this is java code

---
+++ demo.java
@@ -0,0 +1,8 @@
+public class demo {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("this is a java code i want to push it into UpperCaseProgram Repository");
+
+    }
+}
